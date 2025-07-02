de201cefcefe0ec4a1f615dc5ae2839452169559
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-07-02 10:49:12 +0600

    create user

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -9,18 +9,34 @@ import { ApiError } from '../utils/ApiError';
 // Create User 
 const createUser = async (req: Request, res: Response) => {
     const body = req.body;
-    const {name, email, password,} = req.body;
+    const {name, email, password,} = body;
 
     if(name === "" || email === "" || password === ""){
         throw new ApiError(500)
     }
 
+   const existUser = await User.findOne({
+        $or:[{email}]
+    })
 
-    const data = await User.create(body)
+    if(existUser){
+        throw new ApiError(409, "User With email Already Exist")
+    }
+
+    const user = await User.create(body)
+
+    const createdUser = await User.findById(user._id).select(
+        "-password"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "Something is wrong while create user")
+    }
+
     res.status(200).json({
         success: true,
         massage: "Create User Successfully",
-        data
+        data : createdUser
     })
 }
 
